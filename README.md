cfeef6bd13ed484fd2719a9307a6bb443728bdf4
Fernando Henrique Simaro <fernandoh01@gmail.com>
2021-11-24 07:01:35 -0300

    Iniital project structure

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# vue-cli-with-bootstrap
-Template for VueJS 2.6 with Bootstrap-Vue 2.21.2
+# Template for VueJS project using BootstrapVue
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/About.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="about">
+    <b-container fluid>
+      <b-row>
+        <h1>
+          Program here is to easy...
+          <b-badge pill variant="primary">Primary</b-badge>
+        </h1>
+      </b-row>
+      <b-row>
+        <b-col>
+          <div>
+            <label for="example-datepicker">Choose a date</label>
+            <b-form-datepicker
+              id="example-datepicker"
+              v-model="value"
+              class="mb-2"
+            ></b-form-datepicker>
+            <p>Value: '{{ value }}'</p>
+          </div>
+        </b-col>
+        <b-col>
+          <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+            <b-dropdown-item>First Action</b-dropdown-item>
+            <b-dropdown-item>Second Action</b-dropdown-item>
+            <b-dropdown-item>Third Action</b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item active>Active action</b-dropdown-item>
+            <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+          </b-dropdown>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <form class="form-inline">
+            <div class="form-group mb-2">
+              <label for="staticEmail2" class="sr-only">Email</label>
+              <input
+                type="text"
+                readonly
+                class="form-control-plaintext"
+                id="staticEmail2"
+                value="email@example.com"
+              />
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <label for="inputPassword2" class="sr-only">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="inputPassword2"
+                placeholder="Password"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">
+              Confirm identity
+            </button>
+          </form>
+        </b-col>
+        <b-col>
+          <div>
+            <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+            <b-modal id="modal-1" title="BootstrapVue">
+              <p class="my-4">Hello from modal!</p>
+            </b-modal>
+          </div>
+        </b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="4">
+          <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="2000"
+            controls
+            indicators
+            background="#ababab"
+            img-width="1024"
+            img-height="480"
+            style="text-shadow: 1px 1px 2px #333"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+          >
+            <!-- Text slides with image -->
+            <b-carousel-slide
+              caption="First slide"
+              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+              img-src="https://picsum.photos/1024/480/?image=52"
+            ></b-carousel-slide>
+
+            <!-- Slides with custom text -->
+            <b-carousel-slide
+              img-src="https://picsum.photos/1024/480/?image=54"
+            >
+              <h1>Hello world!</h1>
+            </b-carousel-slide>
+
+            <!-- Slides with image only -->
+            <b-carousel-slide
+              img-src="https://picsum.photos/1024/480/?image=58"
+            ></b-carousel-slide>
+
+            <!-- Slides with img slot -->
+            <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+            <b-carousel-slide>
+              <template #img>
+                <img
+                  class="d-block img-fluid w-100"
+                  width="1024"
+                  height="480"
+                  src="https://picsum.photos/1024/480/?image=55"
+                  alt="image slot"
+                />
+              </template>
+            </b-carousel-slide>
+
+            <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+            <b-carousel-slide
+              caption="Blank Image"
+              img-blank
+              img-alt="Blank image"
+            >
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse eros felis, tincidunt a tincidunt eget, convallis
+                vel est. Ut pellentesque ut lacus vel interdum.
+              </p>
+            </b-carousel-slide>
+          </b-carousel>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
